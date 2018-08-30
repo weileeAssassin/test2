fabd36d58a155cf579fccd210b9fbd0e01d14a18
weileeAssassin <631406070209@mails.cqjtu.edu.cn>
2018-08-30 18:08:57 +0800

    bolg页面可以咋网页显示

--- test1/urls.py
+++ test1/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('index/', views.index),
     path('login/',include('login.urls')),
+    path('blogpage/',include('blogpage.urls')),
 ]
