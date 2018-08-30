414c9fdb2b9bb1ec475a37a240f7768ca27efbc4
weileeAssassin <631406070209@mails.cqjtu.edu.cn>
2018-08-30 23:50:31 +0800

    uwsgi静态文件解析不成功测试  uwsgi --http :8008 --chdir /root/lea Django/test1/ --wsgi-file test1/wsgi.py --static-map /static=/srv/django/static

--- login/urls.py
+++ login/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 app_name = 'login'
@@ -9,4 +10,4 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
